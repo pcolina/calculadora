6e9576f452896a075bea9958983dd30aa796498c
pcolina <pabloangelcolina@gmail.com>
2019-12-24 00:20:05 +0100

    Creación de proyecto con spring Boot

---
+++ src/test/java/com/sanitas/CalculadoraTests.java
@@ -0,0 +1,13 @@
+package com.sanitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
